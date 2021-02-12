a11a4932f035ba52613bca8c84221953fdf96d1c
HanzoMain1 <kira.gray@rbe.sk.ca>
2021-02-12 16:27:10 +0000

    added continuous gameplay. made he menu a function and added a quit option.

--- main.py
+++ main.py
@@ -4,30 +4,35 @@
 # Date last modified: 21/02/04
 # Name: Kira Gray
 # Description: simple menu for RPG, accepts, confirms valid/invalid imputs.
-# only accepts all lower case or all capital words
+# only accepts all lower case or all capital words. Single words only.
 
-ans = True
-# while loop that has if-elif-else statements inside for each option
-while ans:
-    print("""
-      Go in one of the following directions:
-      -> Left
-      -> Right
-      Do one of the following actions:
-      -> Explore
-      -> Attack
-      -> Heal
-      """)
-    ans = input("What would you like to do? ")
-    if ans == "Left".lower() or ans == "left".upper():
-        print("You go left.")
-    elif ans == "right".lower() or ans == "right".upper():
-        print("You go right.")
-    elif ans == "Explore".lower() or ans == "Explore".upper():
-        print("You look around.")
-    elif ans == "attack".lower() or ans == "attack".upper():
-        print("You attack.")
-    elif ans == "heal".lower() or ans == "heal".upper():
-        print("You heal 20 points.")
-    else:
-        print("Invalid imput!")
+# function with a while loop that has if-elif-else statements 
+# inside for each option. 
+def continuous(ans):
+  while ans:
+      print("""
+        Go in one of the following directions:
+        -> Left
+        -> Right
+        Do one of the following actions:
+        -> Explore
+        -> Attack
+        -> Heal
+        """)
+      ans = input("What would you like to do? ")
+      if ans == "quit".lower() or ans == "quit".upper():
+        break
+      if ans == "Left".lower() or ans == "left".upper():
+          print("You go left.")
+      elif ans == "right".lower() or ans == "right".upper():
+          print("You go right.")
+      elif ans == "Explore".lower() or ans == "Explore".upper():
+          print("You look around.")
+      elif ans == "attack".lower() or ans == "attack".upper():
+          print("You attack.")
+      elif ans == "heal".lower() or ans == "heal".upper():
+          print("You heal 20 points.")
+      else:
+          print("Invalid imput!")
+
+continuous(ans = True)
